ae47de5f75f81dc11ffc440d6f8c08639fa0affa
Mateusz Matejko <matejko.mateusz7456@gmail.com>
2022-06-16 23:34:24 +0200

    pseudo code intentions added

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
+#Ask user for that name
 name = input("Whats your name? ")
-print("hello, ")
+
+#Say hello to the user
+print("hello,")
 print(name)
+
+#comments can be pseudo code - plan for the coding.. 
