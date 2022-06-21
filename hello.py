d7ee0337f5cf8d62f341a5d3d4e07c50e6672a11
Mateusz Matejko <matejko.mateusz7456@gmail.com>
2022-06-21 22:30:52 +0200

    said some bad words with function

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 name = input("Whats your name? ")
 
 #Say hello to the user
-print(" hello,", name)
+print(" hello,", name, "You","Piece","of","cake")
 
 #comments can be pseudo code - plan for the coding..
 #This is pseudo code 
